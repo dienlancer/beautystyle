728c4c2f3bfa26f7ff9c0e9bc2ca3712829780ea
NguyenKimDien <dienit02@gmail.com>
2017-11-07 19:15:37 +0700

    cap nhat

--- wp-content/themes/dienkimtheme/header.php
+++ wp-content/themes/dienkimtheme/header.php
@@ -64,9 +64,9 @@ $instagram_url=$zendvn_sp_settings['instagram_url'];
 $pinterest_url=$zendvn_sp_settings['pinterest_url'];     
 $quantity=0; 
 if(count($arrCart) > 0){
-foreach($arrCart as $cart){    
-    $quantity+=(int)$cart['product_quantity'];
-}
+    foreach($arrCart as $cart){    
+        $quantity+=(int)$cart['product_quantity'];
+    }
 }
 ?>    
 <header class="header">
@@ -75,8 +75,8 @@ foreach($arrCart as $cart){
             <div class="col-lg-2 no-padding"><font color="#ffffff">Tư vấn 24/7:</font>&nbsp;<font color="#bbb"><?php echo $contacted_phone; ?></font></div>
             <div class="col-lg-6 no-padding"><font color="#ffffff">Địa chỉ:</font>&nbsp;<font color="#bbb"><?php echo $address; ?></font></div>
             <div class="col-lg-4 no-padding">
-                    <div class="col-lg-9 no-padding">
-                        <ul class="top-user">
+                <div class="col-lg-9 no-padding">
+                    <ul class="top-user">
                         <?php                                                              
                         if(empty($arrUser)){
                             ?>
@@ -92,71 +92,71 @@ foreach($arrCart as $cart){
                             <?php                                     
                         }
                         ?>       
-                        </ul> 
-                    </div>                    
-                    <div class="col-lg-3 no-padding">
-                        <div class="mini-cart dropdown box-cart cart hidden-xs">
-                            <a href="<?php echo $cart_link; ?>" >
-                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;Giỏ hàng
-                                <span class="cart-total"><?php echo $quantity; ?></span>
-                            </a>                            
-                        </div>
+                    </ul> 
+                </div>                    
+                <div class="col-lg-3 no-padding">
+                    <div class="mini-cart dropdown box-cart cart hidden-xs">
+                        <a href="<?php echo $cart_link; ?>" >
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;Giỏ hàng
+                            <span class="cart-total"><?php echo $quantity; ?></span>
+                        </a>                            
                     </div>
-                    <div class="clr"></div>             
+                </div>
+                <div class="clr"></div>             
             </div>            
         </div>
     </div>
-<div class="bg-header">
-    <div class="container">        
-        <div class="menu">
-            <div class="col-lg-3 no-padding">                
-                <center><a href="<?php echo home_url(); ?>">                
-                    <img src="<?php echo $customizerGlobal->general_section('site-logo');?>" />
-                </a></center>
-            </div>
-            <div class="col-lg-6 no-padding">                                      
-                <div class="box-search">
-                    <form action="<?php echo $search_link; ?>" method="get">
-                        <input type="text" name="q" autocomplete="off" placeholder="Điện thoại, Ipad, Samsung, Iphone..." value="">
-                        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-                    </form>
+    <div class="bg-header">
+        <div class="container">        
+            <div class="menu">
+                <div class="col-lg-3 no-padding">                
+                    <center><a href="<?php echo home_url(); ?>">                
+                        <img src="<?php echo $customizerGlobal->general_section('site-logo');?>" />
+                    </a></center>
+                </div>
+                <div class="col-lg-6 no-padding">                                      
+                    <div class="box-search">
+                        <form action="<?php echo $search_link; ?>" method="get">
+                            <input type="text" name="q" autocomplete="off" placeholder="Điện thoại, Ipad, Samsung, Iphone..." value="">
+                            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                        </form>
+                        <div class="clr"></div>
+                    </div>
                     <div class="clr"></div>
+                    <div class="goi-y-tu-khoa"><span><font color="#00a651">Gợi ý từ khóa:</font></span> Thời trang nam, Thời trang nữ, Balo, túi xách, Mè và bé...</div>
                 </div>
-                <div class="clr"></div>
-                <div class="goi-y-tu-khoa"><span><font color="#00a651">Gợi ý từ khóa:</font></span> Thời trang nam, Thời trang nữ, Balo, túi xách, Mè và bé...</div>
-            </div>
-            <div class="col-lg-3 no-padding">
-                <div class="col-lg-6">
-                    <div class="box">
-                        <div class="icon">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </div>
-                        <div class="text">
+                <div class="col-lg-3 no-padding">
+                    <div class="col-lg-6">
+                        <div class="box">
+                            <div class="icon">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </div>
+                            <div class="text">
 
-                            <span>Tài khoản</span>
+                                <span>Tài khoản</span>
 
-                            <span class="sub">Đơn hàng</span>
+                                <span class="sub">Đơn hàng</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="box">
-                        <div class="icon">
-                            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-                        </div>
-                        <div class="text">
-                            <span><a href="<?php echo $cart_link; ?>">Giỏ hàng (<?php echo $quantity; ?>)</a></span>
-                            <span class="sub">0đ</span>
+                    <div class="col-lg-6">
+                        <div class="box">
+                            <div class="icon">
+                                <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                            </div>
+                            <div class="text">
+                                <span><a href="<?php echo $cart_link; ?>">Giỏ hàng (<?php echo $quantity; ?>)</a></span>
+                                <span class="sub">0đ</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="clr"></div>
+                    <div class="clr"></div>
+                </div>      
+                <div class="clr"></div>      
             </div>      
-            <div class="clr"></div>      
-        </div>      
-    </div>    
-</div>   
-<div class="main-menu-wrapper">
+        </div>    
+    </div>   
+    <div class="main-menu-wrapper">
         <div class="container">
             <div id="smoothmainmenu" class="ddsmoothmenu">
                 <?php     
@@ -185,47 +185,47 @@ foreach($arrCart as $cart){
             <div class="clr"></div>             
         </div>        
     </div>
-<div class="mobilemenu">
-    <div class="container">
-        <div>
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>                   
+    <div class="mobilemenu">
+        <div class="container">
+            <div>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>                   
+                        </div>
+                        <div id="navbar" class="navbar-collapse collapse">
+                            <?php     
+                            $args = array( 
+                                'menu'              => '', 
+                                'container'         => '', 
+                                'container_class'   => '', 
+                                'container_id'      => '', 
+                                'menu_class'        => 'nav navbar-nav', 
+                                'menu_id'           => 'mobile-menu', 
+                                'echo'              => true, 
+                                'fallback_cb'       => 'wp_page_menu', 
+                                'before'            => '', 
+                                'after'             => '', 
+                                'link_before'       => '', 
+                                'link_after'        => '', 
+                                'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
+                                'depth'             => 3, 
+                                'walker'            => '', 
+                                'theme_location'    => 'mobile-menu' 
+                            );
+                            wp_nav_menu($args);
+                            ?>             
+                        </div>
                     </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <?php     
-                        $args = array( 
-                            'menu'              => '', 
-                            'container'         => '', 
-                            'container_class'   => '', 
-                            'container_id'      => '', 
-                            'menu_class'        => 'nav navbar-nav', 
-                            'menu_id'           => 'mobile-menu', 
-                            'echo'              => true, 
-                            'fallback_cb'       => 'wp_page_menu', 
-                            'before'            => '', 
-                            'after'             => '', 
-                            'link_before'       => '', 
-                            'link_after'        => '', 
-                            'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
-                            'depth'             => 3, 
-                            'walker'            => '', 
-                            'theme_location'    => 'mobile-menu' 
-                        );
-                        wp_nav_menu($args);
-                        ?>             
-                    </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </div>
     </div>
-</div>
 </header>
 
 
